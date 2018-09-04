59ea7716f013b4674e23a2471f75f759f05111c8
David Hansen <david.hansen.0000@gmail.com>
2018-09-04 01:36:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ function getFirstSelector(selector) {
 function nestedTarget() {
   const myNode=document.queryElementById("nested").querySelectorAll("div");
   for (let i=0; i<myNode.length; i++) {
-    
+    if (myNode)
   }
 }
